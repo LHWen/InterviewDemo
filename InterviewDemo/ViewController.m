2dff5ac43ec22537f6d6a0d48ef888ab5f08cc7e
LHWen <1109070804@qq.com>
2018-11-21 11:13:19 +0800

    Objective-C iOS 面试问题总结与问题验证实践

---
+++ InterviewDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  InterviewDemo
+//
+//  Created by LHWen on 2017/10/17.
+//  Copyright © 2017年 LHWen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
